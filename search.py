549d7f999f7fbb4d1ad61cc5d9943e728eaedf18
Chick3nman <admin@chick3nman.com>
2016-07-29 21:08:52 -0500

    Slight speed increase on 2 conditionals, disabled broken function
    Byte MAX and Byte MIN are not generating numbers that line up with the numbers on Hashkiller, currently investigating the discrepancy.

--- search.py
+++ search.py
@@ -18,19 +18,20 @@ def generate_strings(size):
         for i in range(0, len(alphabet), size):
             yield ''.join(alphabet[i: i + size])
 
-def tsum(hexhash):
-    return sum(int(hexhash[i: i + 2], 16) for i in range(0, len(hexhash), 2))
+#def tsum(hexhash):
+#    return sum(int(hexhash[i: i + 2], 16) for i in range(0, len(hexhash), 2))
 
 def work():
+    # Start both not at 0 and 160 to avoid a lot of startup noise.
     max_ones = 120
     min_ones = 40
-    rand_length = 20
+    rand_length = 12
     i = 0
     for combo in generate_strings(rand_length):
         i += 1
-        if i % 1000000 == 0:
+        if i % 100000000 == 0:
             print "Processed %d hashes." % i
-        clear = combo
+        clear = combo + '-Chick3nman'
         hashhex = hashlib.sha1(clear).hexdigest()
 
         ones_count = bin(int(hashhex, 16))[2:].count('1')
@@ -43,16 +44,16 @@ def work():
             min_ones = ones_count
             print "New Bit MIN Hash Found %s = %s" % (plain, min_ones)
 
-        if hashhex.startswith('ffffffffff'):
+        if hashhex[:9] == "fffffffff":
             print "New MAX Hash Found %s:%s" % (hashhex, clear)
-        elif hashhex.startswith('00000000000'):
+        elif hashhex[:10] == '0000000000':
             print "New MIN Hash Found %s:%s" % (hashhex, clear)
 
-        tsumhex = tsum(hashhex)
-        if tsumhex < 125:
-            print "New Byte MIN Hash Found %s:%s" % (hashhex, clear)
-        elif tsumhex > 4800:
-            print "New Byte MAX Hash Found %s:%s" % (hashhex, clear)
+#        tsumhex = tsum(hashhex)
+#        if tsumhex < 500:
+#            print "New Byte MIN Hash Found %s:%s:%s" % (hashhex, clear, tsumhex)
+#        elif tsumhex > 4700:
+#            print "New Byte MAX Hash Found %s:%s:%s" % (hashhex, clear, tsumhex)
 
 if __name__ == '__main__':
     count = multiprocessing.cpu_count()
